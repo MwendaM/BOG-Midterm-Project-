9ef0e99fe17b7ee6deacad3c6ba069c4292815d6
Mwenda Mbui <mwenda2022@gmail.com>
2022-10-04 11:21:34 -0400

    no changes

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ import fetch from "node-fetch";
 
 
 
-fetch ('https://pokeapi.co/api/v2/pokemon/1')
- .then((resp) => resp.json()
- .then((data) => console.log(data)));
+// fetch ('https://pokeapi.co/api/v2/pokemon/1')
+//  .then((resp) => resp.json()
+//  .then((data) => console.log(data)));
 
-
+if ( 2 === "2") {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
